808fcfa0e68cea5423049fae67bc07e91d47a70f
samturton2 <STurton@spartaglobal.com>
2020-11-05 10:24:30 +0000

    __init__ method added

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 ### OOP Object Oriented Programming
 ## There are 4 pillars
 - Inheritance
+  - **Eliminates redundant code**
+  - use all functions and variables from parent class
+
 - Encapsulation
+  - **Reduce complexity and increase reusability**
+  - 
+
 - Abstraction
+  - **reduce complexity and isolate impact of changes**
+
 - Polymorphism
+  - **Refactor code or case statements**
 
 -What are classes
  - classes are a way to bring data and functionality together
@@ -15,5 +24,11 @@
 - Good naming convention is required
 e.g 
 ```python
-
+class Dog:
+    # defining class variable
+    animal_kind = "canine"
+    
+    # self refers to current class
+    def bark(self):
+        return "woof"
 ```
--- dog_class.py
+++ dog_class.py
@@ -10,18 +10,21 @@ class Dog:
     animal_kind = "canine"
     # defining class variable
 
+    def __init__(self, name, colour): #initialising dog class
+        self.name = name
+        self.colour = colour
+
     # self refers to current class
     def bark(self):
         return "woof"
 
 # In order to execute a class we have to create an object of this class
-fido = Dog() # creating an object of dog class
+fido = Dog("lassie", "brown") # creating an object of dog class
 
 print(fido.animal_kind)
 print(fido.bark())
+print(fido.colour)
+
+# manually change attribute
 fido.animal_kind = "big dog"
 print(fido.animal_kind)
-
-lassie = Dog() # creating an object of dog class
-print(lassie.bark())
-print(lassie.animal_kind)
